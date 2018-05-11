8a36051a6159a3225a162b92f9f0296620c92466
飞翔的荷兰人 <Fuyuanpeng@users.noreply.github.com>
2018-05-11 21:37:28 +0800

    Create Observer.php

---
+++ app/code/local/FYP/Shipment/Model/Observer.php
@@ -0,0 +1,10 @@
+<?php 
+class FYP_Shipment_Model_Observer 
+{
+    public function sendTrackEmail($observer)
+	{
+    	$track = $observer->getEvent()->getTrack();
+    	$shipment = $track->getShipment(true);
+    	$shipment->sendEmail();
+	}
+}
