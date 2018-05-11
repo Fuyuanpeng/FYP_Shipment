d02174ffb693ab8949201960ab217fa1b3cf7c66
飞翔的荷兰人 <Fuyuanpeng@users.noreply.github.com>
2018-05-11 21:46:03 +0800

    Update Observer.php

--- app/code/local/FYP/Shipment/Model/Observer.php
+++ app/code/local/FYP/Shipment/Model/Observer.php
@@ -1,8 +1,7 @@
 <?php 
 class FYP_Shipment_Model_Observer 
 {
-    public function sendTrackEmail($observer)
-	{
+    public function sendTrackEmail($observer){
     	$track = $observer->getEvent()->getTrack();
     	$shipment = $track->getShipment(true);
     	$shipment->sendEmail();
